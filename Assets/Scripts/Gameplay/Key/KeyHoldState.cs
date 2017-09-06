1caaa77a46386e730cd04fc9ee012dec9d77cc4f
AdrienMarcenat <adrien.marcenat@gmail.com>
2017-09-06 17:32:48 +0200

    Create chain with distance joint
    A chain is simply several gameobjects with a distance joint
    component linked to the next gameobject. At the end a key can be
    attached to the chain. Now the KeyHoldState has a chain field which can
    be used to correctly handle the chain if the player hold the key : the
    chain will be attached to the player as long as he kept the key, and
    thus the player movement will be constrained.

--- Assets/Scripts/Gameplay/Key/KeyHoldState.cs
+++ Assets/Scripts/Gameplay/Key/KeyHoldState.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class KeyHoldState : FSMState
 {
 	private Transform holderTransform;
+	private Rigidbody2D body;
+	private Rigidbody2D playerBody;
+	[SerializeField] DistanceJoint2D chain;
 
 	protected override void Awake()
 	{
@@ -11,17 +14,34 @@ public class KeyHoldState : FSMState
 		base.Awake ();
 
 		holderTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+		playerBody = GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody2D> ();
+		body = GetComponent<Rigidbody2D> ();
 	}
 
+	public override void Enter ()
+	{
+		transform.parent = holderTransform;
+		body.isKinematic = true;
+		if (chain != null)
+			chain.connectedBody = playerBody;
+	}
+		
 	public override bool UpdateState ()
 	{
-		transform.position = new Vector3(holderTransform.position.x, holderTransform.position.y, transform.position.z);
 		if (Input.GetButtonDown ("Action"))
 			Unhold ();
 		
 		return false;
 	}
 
+	public override void Exit ()
+	{
+		transform.parent = null;
+		body.isKinematic = false;
+		if (chain != null)
+			chain.connectedBody = body;
+	}
+		
 	private void Unhold()
 	{
 		requestStackPop ();
